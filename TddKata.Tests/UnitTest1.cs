6acc4d69df8d19677b5249c11e624e4a077dfd81
A.Matviyko <a.matviyko@creatio.com>
2023-03-06 17:58:02 +0200

    #0

--- TddKata.Tests/UnitTest1.cs
+++ TddKata.Tests/UnitTest1.cs
@@ -10,15 +10,11 @@ namespace TddKata.Tests
     public class UnitTest1
     {
         [Fact]
-        public void AuthorizeUser_ReturnSuccessResult_WhenNewUser()
-        {
+        public void AuthorizeUser_ReturnSuccessResult_WhenNewUser() {
             var authorizationHandler = new AuthorizationHandler();
             var claim = new ClaimsPrincipal();
-
             var unit = authorizationHandler.AuthorizeUser(claim);
-
             unit.IsSuccessful.Should().Be(true);
-
 		}
     }
 }
--- TddKata/AuthorizationHandler.cs
+++ TddKata/AuthorizationHandler.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Claims;
 using LanguageExt;
 using TddKata.Contracts;
+using TddKata.Handlers;
 
 namespace TddKata
 {
     public class AuthorizationHandler
     {
-        public Result<Unit> AuthorizeUser(ClaimsPrincipal user)
-        {
-
-            throw new NotImplementedException();
+        public Result<Unit> AuthorizeUser(ClaimsPrincipal user) {
+            var userId = new UserId(Guid.NewGuid().ToString());
+            var roleId = new RoleId(Guid.NewGuid().ToString());
+            var userRequest = new CreateUserRequest() { UserId = userId, RoleId = roleId };
+            return null;
         }
     }
 }
